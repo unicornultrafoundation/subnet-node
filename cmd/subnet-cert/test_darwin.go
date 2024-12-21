646b683ddabd47fb19b33c77f3158ae36905e2b7
thang <thangn.1411@gmail.com>
2024-12-21 09:54:30 +0700

    remove unused code

--- cmd/subnet-cert/test_darwin.go
+++
@@ -1,4 +0,0 @@
-package main
-
-const NoSuchFileError = "no such file or directory"
-const NoSuchDirError = "no such file or directory"
--- cmd/subnet-cert/test_windows.go
+++
@@ -1,4 +0,0 @@
-package main
-
-const NoSuchFileError = "The system cannot find the file specified."
-const NoSuchDirError = "The system cannot find the path specified."
