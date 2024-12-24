63285906a944b80c50c1635d194de597b34d9c0a
thang <thangn.1411@gmail.com>
2024-12-24 14:22:50 +0700

    update uptime service

--- core/node/uptime.go
+++ core/node/uptime.go
@@ -6,6 +6,7 @@ import (
 	pubsub "github.com/libp2p/go-libp2p-pubsub"
 	"github.com/libp2p/go-libp2p/core/peer"
 	"github.com/unicornultrafoundation/subnet-node/config"
+	"github.com/unicornultrafoundation/subnet-node/core/account"
 	"github.com/unicornultrafoundation/subnet-node/core/apps"
 	"github.com/unicornultrafoundation/subnet-node/core/node/uptime"
 	"github.com/unicornultrafoundation/subnet-node/p2p"
@@ -13,14 +14,15 @@ import (
 	"go.uber.org/fx"
 )
 
-func UptimeService(lc fx.Lifecycle, repo repo.Repo, appService *apps.Service, pubSub *pubsub.PubSub, cfg *config.C, id peer.ID, P2P *p2p.P2P) *uptime.UptimeService {
+func UptimeService(lc fx.Lifecycle, repo repo.Repo, accountService *account.AccountService, appService *apps.Service, pubSub *pubsub.PubSub, cfg *config.C, id peer.ID, P2P *p2p.P2P) *uptime.UptimeService {
 	srv := &uptime.UptimeService{
-		Apps:       appService,
-		IsVerifier: cfg.GetBool("uptime.is_verifier", false),
-		IsProvider: cfg.GetBool("provider.enable", false),
-		Identity:   id,
-		PubSub:     pubSub,
-		Datastore:  repo.Datastore(),
+		Apps:           appService,
+		IsVerifier:     cfg.GetBool("uptime.is_verifier", false),
+		IsProvider:     cfg.GetBool("provider.enable", false),
+		Identity:       id,
+		PubSub:         pubSub,
+		Datastore:      repo.Datastore(),
+		AccountService: accountService,
 	}
 	lc.Append(fx.Hook{
 		OnStop: func(ctx context.Context) error {
