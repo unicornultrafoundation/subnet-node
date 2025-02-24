0d56e959cc9066efc309fee2007c81e59d3fbc2e
Nguyen Thang <thangn.1411@gmail.com>
2025-02-24 10:46:47 +0700

    Update default_config.go

--- config/default_config.go
+++ config/default_config.go
@@ -1,6 +1,6 @@
 package config
 
-const DefaultRPC = "https://rpc-nebulas-testnet.u2u.xyz"
+const DefaultRPC = "https://rpc-mainnet.u2u.xyz"
 const DefaultSubnetAppStoreAddr = "0x5F49358D7717D001Cd5B8bF5613b9bc14cE3dBd2"
 const DefaultSubnetProviderAddr = "0x251240b2Cfbb877Da294893939A8fCaACB414273"
 const DefaultSubnetVerifier = "0x50a734427B851aec24F28326818B869ec83D1732"
