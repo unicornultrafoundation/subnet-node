e610eb7017718bb6dde663c15d46bb346daf32e7
huongnguyenduc <duchuong007@gmail.com>
2025-04-22 18:14:13 +0700

    chore: add new stream manager (WIP)

--- core/vpn/router/packet_worker.go
+++ core/vpn/router/packet_worker.go
@@ -5,7 +5,11 @@ import (
 	"sync"
 	"sync/atomic"
 	"time"
+
+	"github.com/sirupsen/logrus"
 )
+
+var workerLog = logrus.WithField("service", "vpn-packet-worker")
 
 // PacketWorker processes packets
 type PacketWorker struct {
@@ -29,14 +33,14 @@ func (w *PacketWorker) Start() {
 
 // run is the main processing loop for a worker
 func (w *PacketWorker) run() {
-	workerLog := log.WithField("worker_id", w.id)
+	workerLog := workerLog.WithField("worker_id", w.id)
 	workerLog.Debug("Worker started")
-	
+
 	defer func() {
 		w.running = false
 		workerLog.Debug("Worker stopped")
 	}()
-	
+
 	for {
 		select {
 		case <-w.ctx.Done():
@@ -45,9 +49,9 @@ func (w *PacketWorker) run() {
 			if !ok {
 				return
 			}
-			
+
 			err := w.processPacket(task)
-			
+
 			if task.doneCh != nil {
 				task.doneCh <- err
 				close(task.doneCh)
@@ -64,36 +68,64 @@ func (w *PacketWorker) processPacket(task *PacketTask) error {
 		atomic.AddInt64(&w.errorCount, 1)
 		return err
 	}
-	
+
 	// Write packet to stream
 	_, err = stream.Write(task.packet)
 	if err != nil {
-		// Handle stream error
+		// Log the stream error
+		workerLog.WithFields(logrus.Fields{
+			"worker_id": w.id,
+			"peer_id":   task.route.peerID.String(),
+			"index":     task.route.streamIndex,
+			"error":     err,
+		}).Warn("Stream write failed, closing unhealthy stream")
+
+		// Close the unhealthy stream
 		w.router.releaseStream(task.route.peerID, task.route.streamIndex)
 		atomic.AddInt64(&w.errorCount, 1)
-		
-		// Try again with a new stream
+
+		// Try again with a new stream after a short delay to allow for stream creation
+		time.Sleep(10 * time.Millisecond)
 		stream, err = w.router.getStreamForRoute(task.route)
 		if err != nil {
+			workerLog.WithFields(logrus.Fields{
+				"worker_id": w.id,
+				"peer_id":   task.route.peerID.String(),
+				"index":     task.route.streamIndex,
+				"error":     err,
+			}).Error("Failed to get replacement stream")
 			atomic.AddInt64(&w.errorCount, 1)
 			return err
 		}
-		
+
+		// Try writing with the new stream
 		_, err = stream.Write(task.packet)
 		if err != nil {
+			workerLog.WithFields(logrus.Fields{
+				"worker_id": w.id,
+				"peer_id":   task.route.peerID.String(),
+				"index":     task.route.streamIndex,
+				"error":     err,
+			}).Error("Failed to write with replacement stream")
 			atomic.AddInt64(&w.errorCount, 1)
 			return err
 		}
+
+		workerLog.WithFields(logrus.Fields{
+			"worker_id": w.id,
+			"peer_id":   task.route.peerID.String(),
+			"index":     task.route.streamIndex,
+		}).Debug("Successfully wrote packet with replacement stream")
 	}
-	
+
 	// Update metrics
 	atomic.AddInt64(&w.packetCount, 1)
 	w.mu.Lock()
 	w.lastActivity = time.Now()
 	w.mu.Unlock()
-	
-	// Update router metrics
-	w.router.recordPacket(task.route)
-	
+
+	// Update router metrics with packet size
+	w.router.recordPacket(task.route, len(task.packet))
+
 	return nil
 }
